a0940d40855305f8d8b1b81fdc0a09e912ac387f
dfhermont <daniel.hernandez@endava.com>
2022-09-06 14:26:47 -0500

    creating database models

---
+++ models.py
@@ -0,0 +1,31 @@
+from mongoengine import *
+
+class User(Document):
+    email=EmailField(Required=True)
+    meta = {
+        'collection': 'user'
+    }
+
+class StockHistory(EmbeddedDocument):
+    value=FloatField(Required=True)
+    timestamp=DateTimeField(Required=True)
+
+class Stock(Document):
+    name=StringField(Required=True)
+    limit=FloatField(Required=True)
+    StockHistory=ListField(EmbeddedDocumentField(StockHistory))
+    meta = {
+        'collection': 'Stock'
+    }
+
+class CryptoHistory(EmbeddedDocument):
+    value=FloatField(Required=True)
+    timestamp=DateTimeField(Required=True)
+
+class Crypto(Document):
+    name=StringField(Required=True)
+    limit=FloatField(Required=True)
+    CryptoHistory=ListField(EmbeddedDocumentField(CryptoHistory))
+    meta = {
+        'collection': 'Crypto'
+    }
