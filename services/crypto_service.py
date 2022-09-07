5069668bbdf98acaf088c823bbacb9e07f1481c1
jdruedaa <jd.ruedaa@uniandes.edu.co>
2022-09-07 18:13:33 -0500

    Added Crypto Currencies Retrieval from External API

---
+++ services/crypto_service.py
@@ -0,0 +1,62 @@
+import datetime
+import json
+import os
+import time
+from requests import Request, Session
+from threading import Thread
+from dotenv import load_dotenv
+from constants import API_UPDATE_INTERVAL
+
+class CryptoService(Thread):
+
+    def __init__(self) :
+        Thread.__init__(self)
+        load_dotenv()
+        self.url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+        self.parameters = { 'slug': 'bitcoin,ethereum,maker', 'convert': 'USD' }
+        self.headers = {
+            'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY': os.environ['CRYPTO_API_KEY']
+        }
+        self.session = Session()
+        self.session.headers.update(self.headers)
+        self.updating_cryptos=False
+        self.update_interval=API_UPDATE_INTERVAL
+        self.last_update=None
+        self.last_crypto_values=None
+
+    def start_updating_cryptos(self):
+        self.updating_cryptos=True
+        return super().start()
+
+    def get_user_crypto_values(self):
+        crypto_updates=[]
+        response = self.session.get(self.url, params=self.parameters)
+        data= json.loads(response.text)
+        for crypto in data['data']:
+            crypto_name= data['data'][crypto]['name']
+            current_price= data['data'][crypto]['quote']['USD']['price']
+            crypto_updates.append({
+                'name':crypto_name,
+                'price':current_price
+            })
+        return crypto_updates
+
+    def run(self):
+        while(self.updating_cryptos):
+            crypto_updates=self.get_user_crypto_values()
+            if(self.last_update):
+                self.last_update=self.last_update+datetime.timedelta(seconds=self.update_interval)
+            else:
+                self.last_update=datetime.datetime.utcnow()
+            for crypto_update in crypto_updates:
+                crypto_update['timestamp']=self.last_update
+            self.last_crypto_values=crypto_updates
+            print(crypto_updates)
+            time.sleep(self.update_interval)
+    
+    def stop_updating_cryptos(self):
+        self.updating_cryptos=False
+            
+crypto_service=CryptoService()
+crypto_service.start_updating_cryptos()
